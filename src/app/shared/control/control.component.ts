8acab041731f1cc4d6e32cee3deffb1b88b9bc8d
vivianehw <vivianehw@gmail.com>
2024-06-17 19:17:58 -0300

    add ticket, button and form control components

---
+++ src/app/shared/control/control.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-control',
+  standalone: true,
+  imports: [],
+  templateUrl: './control.component.html',
+  styleUrl: './control.component.css',
+  encapsulation: ViewEncapsulation.None,
+})
+export class ControlComponent {
+  @Input({ required: true }) label!: string;
+}
